8a86dfc762f137621665c42c2d0beb89308f0a98
EdwardoSunny <edward.sun2015@gmail.com>
2021-01-26 00:56:24 -0800

    organize feeder

---
+++ src/main/java/team3647/frc2020/commands/OrganizeFeeder.java
@@ -0,0 +1,11 @@
+package team3647.frc2020.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import team3647.frc2020.subsystems.HotDogIndexer;
+
+public class OrganizeFeeder extends SequentialCommandGroup {
+
+    public OrganizeFeeder(HotDogIndexer indexer) {
+        super(new TunnelOut(indexer), new TunnelIn(indexer));
+    }
+}
